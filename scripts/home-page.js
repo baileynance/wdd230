a3eee5a76cb0c539afbaa4d32e2d42671ae8460a
baileynance <baieyj.nance@gmail.com>
2024-03-08 17:57:09 -0800

    added dark mode

--- scripts/home-page.js
+++ scripts/home-page.js
@@ -5,3 +5,18 @@ hambutton.addEventListener('click', () => {
 	mainnav.classList.toggle('show');
 	hambutton.classList.toggle('show');
 });
+
+const modeButton = document.querySelector("#mode");
+const main = document.querySelector("main");
+
+modeButton.addEventListener("click", () => {
+	if (modeButton.textContent.includes("Dark Mode")) {
+        main.style.background = '#000000';
+        main.style.color = "#FFFFFF";
+		modeButton.textContent = "Light Mode";
+	} else {
+		main.style.background = "#FFFFFF";
+        main.style.color = '#2E4052';
+		modeButton.textContent = "Dark Mode";
+	}
+});
