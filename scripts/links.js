9499e51eb727f4a2252f021e0e1cad8aa714f7be
baileynance <baieyj.nance@gmail.com>
2024-03-27 22:51:45 -0700

    dynamic urls

--- scripts/links.js
+++ scripts/links.js
@@ -1,10 +1,24 @@
 const pageURL = "https://baileynance.github.io/wdd230/";
 const linksURL = "https://baileynance.github.io/wdd230/data/links.json";
+const learningActivites = document.getElementById("home-la");
 
 async function getLinks() {
     const response = await fetch(linksURL);
     const data = await response.json();
-    console.log(data);
+    //console.log(data);
+    displayLinks(data.weeks);
+}
+
+const displayLinks = (weeks) => {
+    weeks.forEach((week) => {
+        let activity = document.createElement("li");
+        let links = "";
+        week.links.forEach((link) => {
+            links += `<a href="${link.url}">${link.title}</a>`;
+        })
+        activity.innerHTML = `${week.weeks}: ${links}`;
+        learningActivites.appendChild(activity);
+    })
 }
 
 getLinks();
