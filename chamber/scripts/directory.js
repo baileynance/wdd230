a40036a93db83de61a0e67c64a6577fe4d37014c
baileynance <baieyj.nance@gmail.com>
2024-04-08 12:59:22 -0700

    adjustments

--- chamber/scripts/directory.js
+++ chamber/scripts/directory.js
@@ -1,19 +1,21 @@
 // Grid vs List Styling
 
-const gridbutton = document.querySelector("#grid");
-const listbutton = document.querySelector("#list");
-const display = document.querySelector("#directory-grid");
+const gridbutton = document.getElementById('grid');
+const listbutton = document.getElementById('list');
+const display = document.getElementById('directory-grid');
 
 gridbutton.addEventListener("click", () => {
-	display.classList.add("directory-grid");
-	display.classList.remove("directory-list");
+	display.classList.add("grid");
+	display.classList.remove("list");
 });
 
-listbutton.addEventListener("click", showList);
+listbutton.addEventListener("click", () => {
+	display.classList.add("list");
+	display.classList.remove("grid");
+})
 
-function showList() {
-	display.classList.add("directory-list");
-	display.classList.remove("directory-grid");
+const showList = () => {
+    display.classList.add("grid");
 }
 
 // Dynamic Links
@@ -28,8 +30,7 @@ async function getMembers() {
 
 const displayMembers = (members) => {
     members.forEach((member) => {
-        let section = document.createElement("section");
-        display.appendChild(section);
+        let article = document.createElement("article");
 
         let name = document.createElement("h3");
         name.textContent = member.name;
@@ -40,20 +41,25 @@ const displayMembers = (members) => {
         let url = document.createElement("a");
         url.textContent = member.url;
         let img = document.createElement("img");
-        img.textContent = member.image;
+        img.setAttribute('src', member.image);
+        img.setAttribute('alt', 'Member Logo');
+        img.setAttribute('loading', 'lazy');
         let membership = document.createElement("p");
         membership.textContent = member.membership;
         let other = document.createElement("p");
         other.textContent = member.other;
 
-        section.appendChild(name);
-        section.appendChild(address);
-        section.appendChild(phoneNumber);
-        section.appendChild(url);
-        section.appendChild(img);
-        section.appendChild(membership);
-        section.appendChild(other);
+        article.appendChild(name);
+        article.appendChild(address);
+        article.appendChild(phoneNumber);
+        article.appendChild(url);
+        article.appendChild(img);
+        article.appendChild(membership);
+        article.appendChild(other);
+
+        display.appendChild(article);
     })
 }
 
+showList();
 getMembers();
